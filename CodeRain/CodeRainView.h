8a9aa4b5d4a9215ba205263efd7e8d11c06a15b9
jonshowing <zshowing@gmail.com>
2016-10-06 10:21:23 +0800

    Initial Commit

---
+++ CodeRain/CodeRainView.h
@@ -0,0 +1,13 @@
+//
+//  CodeRainView.h
+//  CodeRain
+//
+//  Created by Shuo Zhang on 2016/10/6.
+//  Copyright © 2016年 Jon Showing. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface CodeRainView : ScreenSaverView
+
+@end
---
+++ CodeRain/CodeRainView.m
@@ -0,0 +1,52 @@
+//
+//  CodeRainView.m
+//  CodeRain
+//
+//  Created by Shuo Zhang on 2016/10/6.
+//  Copyright © 2016年 Jon Showing. All rights reserved.
+//
+
+#import "CodeRainView.h"
+
+@implementation CodeRainView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
